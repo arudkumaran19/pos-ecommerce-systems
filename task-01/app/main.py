ce1cc23e6b209e3c33858f18c77d714b88f0a77f
Arudkumaran Vengadeswaran <arudkumaran19@gmail.com>
2026-09-14 02:22:42 +0530

    chore: prepare Task 01 for deployment

--- task-01/app/main.py
+++ task-01/app/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
@@ -59,9 +60,16 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+
+frontend_url = os.getenv(
+    "FRONTEND_URL",
+    "http://localhost:5173",
+)
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
+        frontend_url,
         "http://localhost:5173",
         "http://127.0.0.1:5173",
     ],
@@ -69,6 +77,10 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+
+app.include_router(products_router)
+
 
 app.include_router(products_router)
 app.include_router(carts_router)
